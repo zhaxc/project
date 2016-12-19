0dfb98ee23b44dac0af8b0dd43439482c410d776
zhaxc <zhaxc0919@aliyun.com>
2016-12-19 09:28:11 +0800

    company

---
+++ shiro-master/shiro3-authorization/src/test/java/com/github/BaseTest.java
@@ -0,0 +1,44 @@
+package com.github;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.apache.shiro.util.ThreadContext;
+import org.junit.After;
+
+import junit.framework.Assert;
+
+public class BaseTest {
+	
+	@After
+	public void tearDown(){
+		//退出时解除绑定到线程 否则对下次测试造成
+		ThreadContext.unbindSubject();
+	}
+	
+	protected void login(String configFile,String username,String password){
+		//1.获取SecurityManager工厂，此处使用ini配置文件初始化SecurityManager
+		Factory<SecurityManager> factory = new IniSecurityManagerFactory(configFile);
+		
+		//获得securityManager实例并绑定给SecurityUtils
+		SecurityManager securityManager = factory.getInstance();
+		SecurityUtils.setSecurityManager(securityManager);
+		
+		//得到主体 subject 创建用户名/密码身份验证token
+		Subject subject =  getSubject();
+		UsernamePasswordToken token = new UsernamePasswordToken(username,password);
+		
+		//登陆
+		subject.login(token);
+		
+	}
+	
+	public Subject getSubject() {
+		return SecurityUtils.getSubject();
+	}
+	
+}
